89da2c98fdbd2e64eeddf02664f86a9c3f1d442a
Curtis McEnroe <programble@gmail.com>
2011-04-09 13:08:38 -0400

    Trying to get server replies

--- lib/server.rb
+++ lib/server.rb
@@ -13,7 +13,7 @@ class Server
   
   def broadcast(data)
     @clients.each do |client|
-      @socket.send(data, 0, client[2], client[1])
+      @socket.send(data, 0, client[3], client[1])
     end
   end
   
