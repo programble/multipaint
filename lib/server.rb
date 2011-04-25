5276306502f564a81590759d5e91c395cac07d06
Curtis McEnroe <programble@gmail.com>
2011-04-25 14:45:01 -0400

    Don't send empty erase thingies

--- lib/server.rb
+++ lib/server.rb
@@ -55,8 +55,9 @@ class Server
         puts "DRAW #{sender[3]}/#{@nicks[sender]} #{x} #{y} #{color}"
       when Commands::ERASE
         x, y = data.unpack('n n n')[1..2]
-        puts "ERASE #{sender[3]}/#{@nicks[sender]} #{x} #{y}"
         erased = @points.select {|point| point[0] < x + 10 && point[0] > x - 10 && point[1] < y + 10 && point[1] > y - 10}
+        next if erased.empty?
+        puts "ERASE #{sender[3]}/#{@nicks[sender]} #{x} #{y}"
         erased.each {|point| @points.delete(point)}
         broadcast([Commands::ERASE, @nicks[sender], *erased.flatten].pack('n Z* n*'))
       when Commands::PINGPONG
