7364ce5737aa9b46d586ae4baf7e1906b7db01c6
Curtis McEnroe <programble@gmail.com>
2011-04-24 21:39:10 -0400

    I FIX I FIX

--- lib/server.rb
+++ lib/server.rb
@@ -10,6 +10,7 @@ class Server
     @socket.setsockopt(Socket::SOL_SOCKET, Socket::SO_BROADCAST, true)
     
     @clients = []
+    @nicks = {}
     @points = []
   end
   
@@ -23,44 +24,44 @@ class Server
     while true
       data, sender = @socket.recvfrom(65536)
       command = data.unpack('n')[0]
-      if !@clients.map{|x|x[0..3]}.include?(sender) && command != Commands::CONNECT
+      if !@clients.include?(sender) && command != Commands::CONNECT
         puts "#{data.inspect} #{sender[3]} (IGNORED)"
         next
       end
       
       case command
       when Commands::CONNECT
-        sender << data.unpack('n Z*')[1]
+        @nicks[sender] = data.unpack('n Z*')[1]
         @clients << sender unless @clients.include?(sender)
-        broadcast([Commands::CONNECT, sender[4]].pack('n a*x'))
+        broadcast([Commands::CONNECT, @nicks[sender]].pack('n a*x'))
         @points.each do |point|
           @socket.send([Commands::DRAW, '', point[0], point[1], point[2]].pack('n Z* n n n'), 0, Addrinfo.new(sender[0..3]))
         end
-        puts "CONNECT #{sender[3]}/#{sender[4]}"
+        puts "CONNECT #{sender[3]}/#{@nicks[sender]}"
       when Commands::DISCONNECT
         @clients.delete(sender)
-        broadcast([Commands::DISCONNECT, sender[4]].pack('n Z*'))
-        puts "DISCONNECT #{sender[3]}/#{sender[4]}"
+        broadcast([Commands::DISCONNECT, @nicks[sender]].pack('n Z*'))
+        puts "DISCONNECT #{sender[3]}/#{@nicks[sender]}"
       when Commands::MESSAGE
         message = data.unpack('n Z*')[1]
-        broadcast([Commands::MESSAGE, sender[4], message].pack('n Z* Z*'))
-        puts "MESSAGE #{sender[3]}/#{sender[4]} #{message}"
+        broadcast([Commands::MESSAGE, @nicks[sender], message].pack('n Z* Z*'))
+        puts "MESSAGE #{sender[3]}/#{@nicks[sender]} #{message}"
       when Commands::DRAW
         x, y, color = data.unpack('n n n n')[1..3]
         next if @points.include?([x,y,color])
         @points << [x, y, color]
-        broadcast([Commands::DRAW, sender[4], x, y, color].pack('n Z* n n n'))
-        puts "DRAW #{sender[3]}/#{sender[4]} #{x} #{y} #{color}"
+        broadcast([Commands::DRAW, @nicks[sender], x, y, color].pack('n Z* n n n'))
+        puts "DRAW #{sender[3]}/#{@nicks[sender]} #{x} #{y} #{color}"
       when Commands::ERASE
         x, y = data.unpack('n n n')[1..2]
         erased = @points.select {|point| point[0] < x + 10 && point[0] > x - 10 && point[1] < y + 10 && point[1] > y - 10}
         erased.each do |point|
           @points.delete(point)
-          broadcast([Commands::ERASE, sender[4], point[0], point[1], point[2]].pack('n Z* n n n'))
-          puts "ERASE #{sender[3]}/#{sender[4]} #{point[0]} #{point[1]}"
+          broadcast([Commands::ERASE, @nicks[sender], point[0], point[1], point[2]].pack('n Z* n n n'))
+          puts "ERASE #{sender[3]}/#{@nicks[sender]} #{point[0]} #{point[1]}"
         end
       when Commands::PINGPONG
-        puts "PING #{sender[3]}/#{sender[4]} #{data.inspect}"
+        puts "PING #{sender[3]}/#{@nicks[sender]} #{data.inspect}"
         @socket.send(data, 0, Addrinfo.new(sender[0..3]))
       end
     end
